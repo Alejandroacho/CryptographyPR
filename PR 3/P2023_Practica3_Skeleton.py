466eddead06afe72305f9e570b619cfbd2c18bc2
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2023-05-21 18:27:35 +0200

    feat: Implement ComputePoints

--- PR 3/P2023_Practica3_Skeleton.py
+++ PR 3/P2023_Practica3_Skeleton.py
@@ -8,7 +8,21 @@ P_INFINITY = (None, None)
 # --- IMPLEMENTATION GOES HERE -----------------------------------------------
 #  Student helpers (functions, constants, etc.) can be defined here, if needed
 
+def is_quadratic_residue(n, p):
+    return pow(n, (p - 1) // 2, p) == 1
 
+def modular_sqrt(n, p):
+    if p % 4 == 3:
+        return pow(n, (p + 1) // 4, p)
+    elif p % 8 == 5:
+        v = pow(2 * n, (p - 5) // 8, p)
+        i = pow(2 * n * v * v, (p - 1) // 4, p)
+        return (n * v * (i - 1) // 2) % p
+    else:
+        # En otros casos, se puede utilizar fuerza bruta para encontrar la raíz cuadrada
+        for i in range(p):
+            if i * i % p == n:
+                return i
 # ----------------------------------------------------------------------------
 
 
@@ -24,7 +38,20 @@ def uoc_ComputePoints(curve):
 
     #### IMPLEMENTATION GOES HERE ####
 
-
+    # We first get the values of the curve
+    a = curve[0]
+    b = curve[1]
+    p = curve[2]
+    # We add the Pinfinity point
+    num_points = num_points+ 1
+    # We iterate over all the possible values of p over x and y
+    for x in range(p):
+        for y in range(p):
+            # We check if the point is on the curve by checking if
+            # y^2 (mod p) = x^3 + ax + b (mod p)
+            if (y * y) % p == (x * x * x + a * x + b) % p:
+                # If it is, we add it to the number of points
+                num_points += 1
     # --------------------------------
 
     return num_points
